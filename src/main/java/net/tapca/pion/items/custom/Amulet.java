45823a32d6942457de142e9fa730c1e6143bcaa4
SomeKindOfCoder <alexvenezia1@gmail.com>
2022-12-04 17:28:18 +0000

    Update Amulet.java

--- src/main/java/net/tapca/pion/items/custom/Amulet.java
+++ src/main/java/net/tapca/pion/items/custom/Amulet.java
@@ -1,23 +1,12 @@
 package net.tapca.pion.items.custom;
 
 import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.entity.player.PlayerInventory;
-import net.minecraft.entity.player.ServerPlayerEntity;
-import net.minecraft.inventory.container.Container;
-import net.minecraft.inventory.container.INamedContainerProvider;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.Hand;
-import net.minecraft.util.IItemProvider;
-import net.minecraft.util.text.ITextComponent;
-import net.minecraft.util.text.TranslationTextComponent;
 import net.minecraft.world.World;
-import net.minecraftforge.fml.network.NetworkHooks;
-import net.tapca.pion.containers.AmuletContainer;
 import top.theillusivec4.curios.api.type.capability.ICurioItem;
-
-import javax.annotation.Nullable;
 
 public class Amulet extends Item implements ICurioItem {
 
@@ -28,7 +17,6 @@ public class Amulet extends Item implements ICurioItem {
 
     @Override
     public ActionResult<ItemStack> onItemRightClick(World worldIn, PlayerEntity playerIn, Hand handIn) {
-
         return super.onItemRightClick(worldIn, playerIn, handIn);
     }
 }

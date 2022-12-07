238115098a1c1f46cc071c88d6b29dbda1964657
Quintus4718 <quintus4718@gmail.com>
2022-12-07 21:02:46 +0000

    sproutling behavior changes

--- src/main/java/net/tapca/pion/entities/custom/SproutlingEntity.java
+++ src/main/java/net/tapca/pion/entities/custom/SproutlingEntity.java
@@ -2,40 +2,50 @@ package net.tapca.pion.entities.custom;
 
 
 import net.minecraft.block.BlockState;
-import net.minecraft.client.Minecraft;
-import net.minecraft.client.particle.FlameParticle;
+import net.minecraft.entity.AgeableEntity;
 import net.minecraft.entity.CreatureEntity;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.MobEntity;
 import net.minecraft.entity.ai.attributes.AttributeModifierMap;
 import net.minecraft.entity.ai.attributes.Attributes;
 import net.minecraft.entity.ai.goal.*;
-import net.minecraft.entity.passive.ChickenEntity;
+import net.minecraft.entity.passive.AnimalEntity;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.Items;
+import net.minecraft.item.crafting.Ingredient;
 import net.minecraft.util.DamageSource;
 import net.minecraft.util.SoundEvent;
 import net.minecraft.util.SoundEvents;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.world.IWorldReader;
 import net.minecraft.world.World;
+import net.minecraft.world.server.ServerWorld;
 
-public class SproutlingEntity extends CreatureEntity {
+import javax.annotation.Nullable;
+
+public class SproutlingEntity extends AnimalEntity {
 
     public SproutlingEntity(EntityType<? extends CreatureEntity> type, World worldIn) {
-        super(type, worldIn);
+        super((EntityType<? extends AnimalEntity>) type, worldIn);
 
     }
 
     public static AttributeModifierMap.MutableAttribute setCustomAttributes() {
         return MobEntity.func_233666_p_()
                 .createMutableAttribute(Attributes.MAX_HEALTH, 10.0D)
-                .createMutableAttribute(Attributes.MOVEMENT_SPEED, 0.3D);
+                .createMutableAttribute(Attributes.MOVEMENT_SPEED, 0.5D);
     }
 
     @Override
     protected void registerGoals() {
         super.registerGoals();
-        this.goalSelector.addGoal(1, new RandomWalkingGoal(this, 0.7D, 1));
+        this.goalSelector.addGoal(0, new SwimGoal(this));
+        this.goalSelector.addGoal(1, new PanicGoal(this, 0.7D));
+        this.goalSelector.addGoal(2, new BreedGoal(this, 0.3));
+        this.goalSelector.addGoal(3, new TemptGoal(this, 0.3D, Ingredient.fromItems(Items.BONE_MEAL), false));
+        this.goalSelector.addGoal(4, new FollowParentGoal(this, 0.3D));
+        this.goalSelector.addGoal(5, new WaterAvoidingRandomWalkingGoal(this, 0.3D));
+        this.goalSelector.addGoal(6, new LookAtGoal(this, PlayerEntity.class, 6.0F));
+        this.goalSelector.addGoal(7, new LookRandomlyGoal(this));
     }
 
     @Override
@@ -61,6 +71,12 @@ public class SproutlingEntity extends CreatureEntity {
     @Override
     protected void playStepSound(BlockPos pos, BlockState blockIn) {
         super.playSound(SoundEvents.ENTITY_ZOGLIN_ANGRY, 0.15F, 1.0F);
+    }
+
+    @Nullable
+    @Override
+    public AgeableEntity createChild(ServerWorld world, AgeableEntity mate) {
+        return null;
     }
 
     @Override
